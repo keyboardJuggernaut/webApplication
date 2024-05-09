e0fabb5fbdaf5828f68bfd266e897efab4028853
keyboardJuggernaut <riccardomagni00@gmail.com>
2024-05-09 20:09:27 +0200

    feat: error page

--- src/main/java/it/polimi/parkingService/webApplication/AppExceptionHandler.java
+++ src/main/java/it/polimi/parkingService/webApplication/AppExceptionHandler.java
@@ -5,12 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @ControllerAdvice
 public class AppExceptionHandler {
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleException(Exception exc) {
+    public String handleException(Exception exc) {
         exc.printStackTrace();
         // create a ErrorResponse
         ErrorResponse error = new ErrorResponse();
@@ -20,6 +21,26 @@ public class AppExceptionHandler {
         error.setTimeStamp(System.currentTimeMillis());
 
         // return ResponseEntity
-        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+        return "error";
     }
+    @RequestMapping("/error")
+    public String handleError() {
+        //do something like logging
+        return "error";
+    }
+
+//    @ExceptionHandler
+//    public ResponseEntity<ErrorResponse> handleException(Exception exc) {
+//        exc.printStackTrace();
+//        // create a ErrorResponse
+//        ErrorResponse error = new ErrorResponse();
+//
+//        error.setStatus(HttpStatus.BAD_REQUEST.value());
+//        error.setMessage(exc.getMessage());
+//        error.setTimeStamp(System.currentTimeMillis());
+//
+//        // return ResponseEntity
+//        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+//    }
+
 }
